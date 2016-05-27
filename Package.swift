f3f83242ae38c46f93fd487ea9ba99ef0201321f
noppoman <yuki@miketokyo.com>
2016-05-27 15:26:51 +0900

    Update deps

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "HTTPSClient",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/TCPSSL.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/noppoMan/HTTPParser.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/noppoMan/HTTPSerializer.git", majorVersion: 0, minor: 7),
     ]
 )
